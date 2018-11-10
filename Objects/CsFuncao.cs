9f90e6abf1292126e51dab904a521e1a1bf8907a
Angelo Luiz Perim Antunes <angeloluizperimantunes@hotmail.com>
2018-11-10 15:40:06 -0200

    Término da programação do objeto funcao.

---
+++ Objects/CsFuncao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    public class CsFuncao
+    {
+        #region Atributo
+        private int id;
+        private string nome;
+        private string descricao;
+        #endregion
+        #region Propriedades
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Descricao { get => descricao; set => descricao = value; }
+        #endregion
+        private CsFuncao CsFuncaoDataGridView(System.Data.DataRow line)
+        {
+            CsFuncao csFuncao = new CsFuncao
+            {
+                Id =  Convert.ToInt32(line["id"]),
+                Nome = Convert.ToString(line["nome"]),
+                Descricao = Convert.ToString(line["descricao"])
+            };
+            return csFuncao;
+
+        }
+    }
+}
