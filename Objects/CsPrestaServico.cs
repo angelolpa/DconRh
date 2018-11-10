586723ca5fb866bd272c792c7e9df6cc39a21334
Angelo Luiz Perim Antunes <angeloluizperimantunes@hotmail.com>
2018-11-10 15:15:31 -0200

    Fim da estruturação da entidade associativa PrestaServico, seus atributos foram adicionados, bem como uma Classe para armazenar seus conjuntos.

---
+++ Objects/CsPrestaServico.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Objects
+{
+    public class CsPrestaServico
+    {
+        #region Atributos
+        private int id;
+        private int fkEmpresa;
+        private int fkFuncionario;
+        private DateTime dataRegistro;
+        private DateTime entrada;
+        private DateTime intervalo;
+        private DateTime saida;
+        private DateTime horasTrabalhadas;
+        private DateTime horasExtras;
+        private DateTime totalHoras;
+
+        public int Id { get => id; set => id = value; }
+        public int FkEmpresa { get => fkEmpresa; set => fkEmpresa = value; }
+        public int FkFuncionario { get => fkFuncionario; set => fkFuncionario = value; }
+        public DateTime DataRegistro { get => dataRegistro; set => dataRegistro = value; }
+        public DateTime Entrada { get => entrada; set => entrada = value; }
+        public DateTime Intervalo { get => intervalo; set => intervalo = value; }
+        public DateTime Saida { get => saida; set => saida = value; }
+        public DateTime HorasTrabalhadas { get => horasTrabalhadas; set => horasTrabalhadas = value; }
+        public DateTime HorasExtras { get => horasExtras; set => horasExtras = value; }
+        public DateTime TotalHoras { get => totalHoras; set => totalHoras = value; }
+        #endregion
+
+        #region Auxiliar
+        public CsPrestaServico CsPrestaServicoDataGridView(System.Data.DataRow line)
+        {
+            CsPrestaServico csPrestaServico = new CsPrestaServico
+            {
+                Id = Convert.ToInt32(line["id"]),
+                FkEmpresa = Convert.ToInt32(line["fk_empresa"]),
+                FkFuncionario = Convert.ToInt32(line["fk_funcionario"]),
+                DataRegistro = Convert.ToDateTime(line["data_registro"]),
+                Entrada = Convert.ToDateTime(line["entrada"]),
+                Intervalo = Convert.ToDateTime(line["intervalo"]),
+                Saida = Convert.ToDateTime(line["saida"]),
+                HorasTrabalhadas = Convert.ToDateTime(line["horas_trabalhadas"]),
+                HorasExtras = Convert.ToDateTime(line["horas_extras"]),
+                TotalHoras = Convert.ToDateTime(line["total_horas"])
+            };
+
+            return csPrestaServico;
+        }
+
+        #endregion
+    }
+}
