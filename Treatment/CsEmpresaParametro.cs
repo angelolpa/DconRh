57016505f0fd1107e5d1a49a2e34023a0b2df6e2
Ramon nazaro de oliveira camporêz <ramonnazaro@gmail.com>
2018-11-18 12:24:20 -0200

    Termino da camada de encapsulamento de dados.

---
+++ Treatment/CsEmpresaParametro.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using DataBase;
+using Objects;
+namespace Treatment
+{
+    class CsEmpresaParametro
+    {
+        CsEmpresa csEmpresa;
+        CsCollectionEmpresa csCollectionEmpresa;
+        CsCommand csCommand;
+
+        public void CsEmpresaParemetroColecao(CommandType commandType, string oleDbCommand, CsEmpresa csEmpresa)
+        {
+            csCommand = new CsCommand();
+
+            csCommand.ParameterCollection_Clear();
+
+            csCommand.ParameterCollection_Add("@Id", csEmpresa.Id);
+            csCommand.ParameterCollection_Add("@Nome", csEmpresa.Nome);
+            csCommand.ParameterCollection_Add("@Cnpj", csEmpresa.Cnpj);
+            csCommand.ParameterCollection_Add("@Telefone", csEmpresa.Telefone);
+            csCommand.ParameterCollection_Add("@Endereco", csEmpresa.Endereco);
+
+            csCommand.ExecuteCommandNonQuery(commandType, oleDbCommand);
+        }
+
+
+        public CsCollectionEmpresa CsEmpresaParametroColecao_Returno(CommandType commandType, string oleDbCommand, string where, string name)
+        {
+            csEmpresa = new CsEmpresa();
+            csCollectionEmpresa = new CsCollectionEmpresa();
+            csCommand = new CsCommand();
+
+            csCommand.ParameterCollection_Clear();
+
+            if (!(String.IsNullOrEmpty(name)))
+            {
+                csCommand.ParameterCollection_Add("@Name", name);
+            }
+
+            DataTable dataTable = csCommand.ExecuteCommandConsult(commandType, oleDbCommand);
+
+            foreach (DataRow linha in dataTable.Rows)
+            {
+                csCollectionEmpresa.Add(csEmpresa.CsEmpresaDataGridView(linha));
+            }
+
+            return csCollectionEmpresa;
+        }
+
+
+    }
+}
